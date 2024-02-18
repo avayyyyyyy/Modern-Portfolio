b5fdfaac4bf7ce8504357df94f7e9b62d5805ce9
avayyyyyyy <jabhi465@gmail.com>
2024-02-18 20:00:24 +0530

    Bugs 🦈

--- src/assets/Components/Introduction.jsx
+++ src/assets/Components/Introduction.jsx
@@ -49,7 +49,7 @@ const Introduction = () => {
       <div className="flex justify-between items-center gap-[10px]">
         <a
           className="lg:py-5 z-40 lg:px-6 p-3 rounded-full text-center hover:bg-[#ff924f] text-white bg-[#ff8132] border-2 border-[#ff8132]"
-          href="#projects"
+          href="#project"
         >
           Check My <span className="font-bold">project</span> 🚀
         </a>

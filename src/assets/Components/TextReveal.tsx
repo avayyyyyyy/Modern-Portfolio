dc5b9bc85e51c016a4a513de6c214eb99688623f
avayyyyyyy <jabhi465@gmail.com>
2024-05-19 17:31:16 +0530

    fix: 🐛 Typing

--- src/assets/Components/TextReveal.tsx
+++ src/assets/Components/TextReveal.tsx
@@ -4,7 +4,7 @@ import TextRevealByWord from "./magicui/text-reveal";
 function TextReveal() {
   return (
     <div className="z-10 flex min-h-[16rem] w-full items-center justify-center bg-white">
-      <TextRevealByWord text="I love to build web apps using React, Next.js, Tailwindcss, PostgresSQL and Prisma. 🚀" />
+      <TextRevealByWord text="I love to build web apps using ReactJs, NextJs, TailwindCSS, PostgresSQL and Prisma. 🚀" />
     </div>
   );
 }

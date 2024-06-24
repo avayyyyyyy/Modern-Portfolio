0a7496cce1de43fb58a6fedb15fcbccc9d5d4cef
avayyyyyyy <jabhi465@gmail.com>
2024-06-24 21:44:05 +0530

    refactor: Update ResumeSection component with added target="_blank" for resume download link

--- src/assets/Components/ResumeSection.tsx
+++ src/assets/Components/ResumeSection.tsx
@@ -20,6 +20,7 @@ const ResumeSection = () => {
               to={
                 "https://drive.google.com/uc?export=download&id=1HHD5stjYrvF6ZvfbRuBoQ5JKiUqgIHgq"
               }
+              target="_blank"
               download
               className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-white shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
             >

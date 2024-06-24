bf89b2230d41ec8e04c6c6938363257f89b1a35e
avayyyyyyy <jabhi465@gmail.com>
2024-06-24 21:06:33 +0530

    refactor: Update ProjectCard component with adjusted date styling

--- src/assets/Components/ProjectCard.tsx
+++ src/assets/Components/ProjectCard.tsx
@@ -37,7 +37,9 @@ function ProjectCard({
             )}
             <div className="font-semibold text-sm mt-2 ">{name}</div>
           </div>
-          <div className="text-xs text-zinc-600 mb-2">{date} - Present</div>
+          <div className="text-xs text-zinc-600 mb-2 mt-1">
+            {date} - Present
+          </div>
           <div className="text-xs text-zinc-500">
             {shortDesc.length > 100 ? (
               <div>{shortDesc.split("").splice(0, 100).join("")}...</div>

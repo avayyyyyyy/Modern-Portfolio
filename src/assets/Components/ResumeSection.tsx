b1de0e31df58ccb0b53d35742816cfe28cb78be6
avayyyyyyy <jabhi465@gmail.com>
2024-08-15 23:47:17 +0530

    style(components): update icons and text sizes in ResumeSection component

--- src/assets/Components/ResumeSection.tsx
+++ src/assets/Components/ResumeSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BiDownArrow } from "react-icons/bi";
+import { HiDocumentArrowUp } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 
 const ResumeSection = () => {
@@ -9,11 +9,11 @@ const ResumeSection = () => {
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
-              <div className="text-sm px-3 mb-4 mx-auto py-1 w-fit bg-zinc-200 rounded-md  text-zinc-800">
-                Want my resume?
+              <div className="md:text-sm text-xs px-3 mb-4 mx-auto py-1 w-fit bg-zinc-200 rounded-md  text-zinc-900">
+                Yess, I'm looking for opportunities 😇
               </div>
-              <h1 className="lg:text-6xl text-4xl w-[60%] mx-auto  text-center font-semibold mb-10 ">
-                Click the button below to download
+              <h1 className="lg:text-4xl text-xl w-[70%] mx-auto  text-center font-semibold mb-10 ">
+                Click the button below to download my resume...
               </h1>
             </div>
             <Link
@@ -22,10 +22,10 @@ const ResumeSection = () => {
               }
               target="_blank"
               download
-              className="inline-flex h-10 items-center justify-center rounded-full bg-black hover:bg-black/80 px-8 text-sm font-medium text-white shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+              className="inline-flex items-center justify-center rounded-full bg-black hover:bg-black/80 px-4 py-2 text-sm font-medium text-white shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
             >
-              Download Resume{" "}
-              <BiDownArrow size={16} className="ml-2 rotate-[270deg] " />
+              Download{" "}
+              <HiDocumentArrowUp size={16} className="ml-2 rotate-[180deg] " />
             </Link>
           </div>
         </div>
